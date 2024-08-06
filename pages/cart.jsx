0e3755500b44742d68de500a4b2a41ebba1ccb8a
unknown <khalidnabgaoui4@gmail.com>
2024-08-06 17:46:42 +0100

    optimizations

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -1,82 +1,106 @@
-import Button from "@/components/Button";
-import { CartContext } from "@/components/CartContext";
-import Center from "@/components/Center";
 import Header from "@/components/Header";
-import Input from "@/components/Input";
-import Table from "@/components/Table";
-import axios from "axios";
-import { useContext, useEffect, useState } from "react";
 import styled from "styled-components";
+import Center from "@/components/Center";
+import Button from "@/components/Button";
+import { useContext, useEffect, useState } from "react";
+import { CartContext } from "@/components/CartContext";
+import axios from "axios";
+import Table from "@/components/Table";
+import Input from "@/components/Input";
 
 const ColumnsWrapper = styled.div`
   display: grid;
-  grid-template-columns: 1.3fr 0.7fr;
+  grid-template-columns: 1fr;
+  @media screen and (min-width: 768px) {
+    grid-template-columns: 1.2fr 0.8fr;
+  }
   gap: 40px;
   margin-top: 40px;
 `;
+
 const Box = styled.div`
   background-color: #fff;
   border-radius: 10px;
   padding: 30px;
 `;
+
 const ProductInfoCell = styled.td`
   padding: 10px 0;
-  font-weight: bold;
 `;
+
 const ProductImageBox = styled.div`
-  width: 100px;
+  width: 70px;
   height: 100px;
-  padding: 10px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  padding: 2px;
+  border: 1px solid rgba(0, 0, 0, 0.1);
   display: flex;
   align-items: center;
   justify-content: center;
   border-radius: 10px;
-  margin-bottom: 5px;
   img {
-    max-width: 80px;
-    max-height: 80px;
+    max-width: 60px;
+    max-height: 60px;
+  }
+  @media screen and (min-width: 768px) {
+    padding: 10px;
+    width: 100px;
+    height: 100px;
+    img {
+      max-width: 80px;
+      max-height: 80px;
+    }
   }
 `;
+
 const QuantityLabel = styled.span`
-  padding: 0 3px;
-  font-weight: bolde;
+  padding: 0 15px;
+  display: block;
+  @media screen and (min-width: 768px) {
+    display: inline-block;
+    padding: 0 10px;
+  }
 `;
+
 const CityHolder = styled.div`
   display: flex;
   gap: 5px;
 `;
 
 export default function CartPage() {
-  const { cartProducts, addProduct, removeProduct } = useContext(CartContext);
+  const { cartProducts, addProduct, removeProduct, clearCart } =
+    useContext(CartContext);
   const [products, setProducts] = useState([]);
-
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [city, setCity] = useState("");
   const [postalCode, setPostalCode] = useState("");
   const [streetAddress, setStreetAddress] = useState("");
   const [country, setCountry] = useState("");
-
   const [isSuccess, setIsSuccess] = useState(false);
-
   useEffect(() => {
     if (cartProducts.length > 0) {
-      axios
-        .post("/api/cart", { ids: cartProducts })
-        .then((response) => setProducts(response.data));
+      axios.post("/api/cart", { ids: cartProducts }).then((response) => {
+        setProducts(response.data);
+      });
     } else {
       setProducts([]);
     }
   }, [cartProducts]);
-
+  useEffect(() => {
+    if (typeof window === "undefined") {
+      return;
+    }
+    if (window?.location.href.includes("success")) {
+      setIsSuccess(true);
+      clearCart();
+    }
+  }, []);
   function moreOfThisProduct(id) {
     addProduct(id);
   }
   function lessOfThisProduct(id) {
     removeProduct(id);
   }
-
   async function goToPayment() {
     const response = await axios.post("/api/checkout", {
       name,
@@ -91,15 +115,6 @@ export default function CartPage() {
       window.location = response.data.url;
     }
   }
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      if (window.location.href.includes("success")) {
-        setIsSuccess(true);
-      }
-    }
-  }, []);
-
   let total = 0;
   for (const productId of cartProducts) {
     const price = products.find((p) => p._id === productId)?.price || 0;
@@ -114,26 +129,22 @@ export default function CartPage() {
           <ColumnsWrapper>
             <Box>
               <h1>Thanks for your order!</h1>
-              <p>We will order you when your order will be sent.</p>
+              <p>We will email you when your order will be sent.</p>
             </Box>
           </ColumnsWrapper>
         </Center>
       </>
     );
   }
-
   return (
     <>
       <Header />
-
       <Center>
         <ColumnsWrapper>
           <Box>
             <h2>Cart</h2>
-
             {!cartProducts?.length && <div>Your cart is empty</div>}
-
-            {products.length > 0 && (
+            {products?.length > 0 && (
               <Table>
                 <thead>
                   <tr>
@@ -142,13 +153,12 @@ export default function CartPage() {
                     <th>Price</th>
                   </tr>
                 </thead>
-
                 <tbody>
                   {products.map((product) => (
-                    <tr>
+                    <tr key={product._id}>
                       <ProductInfoCell>
                         <ProductImageBox>
-                          <img src={product.images[0]} alt={product.title} />
+                          <img src={product.images[0]} alt="" />
                         </ProductImageBox>
                         {product.title}
                       </ProductInfoCell>
@@ -166,72 +176,70 @@ export default function CartPage() {
                           +
                         </Button>
                       </td>
-                      <ProductInfoCell>
+                      <td>
                         $
                         {cartProducts.filter((id) => id === product._id)
                           .length * product.price}
-                      </ProductInfoCell>
+                      </td>
                     </tr>
                   ))}
                   <tr>
                     <td></td>
                     <td></td>
-                    <ProductInfoCell>${total}</ProductInfoCell>
+                    <td>${total}</td>
                   </tr>
                 </tbody>
               </Table>
             )}
           </Box>
-
-          {!!cartProducts.length && (
+          {!!cartProducts?.length && (
             <Box>
               <h2>Order information</h2>
               <Input
                 type="text"
                 placeholder="Name"
-                name="name"
                 value={name}
+                name="name"
                 onChange={(ev) => setName(ev.target.value)}
               />
               <Input
                 type="text"
                 placeholder="Email"
-                name="email"
                 value={email}
+                name="email"
                 onChange={(ev) => setEmail(ev.target.value)}
               />
               <CityHolder>
                 <Input
                   type="text"
                   placeholder="City"
-                  name="city"
                   value={city}
+                  name="city"
                   onChange={(ev) => setCity(ev.target.value)}
                 />
                 <Input
                   type="text"
                   placeholder="Postal Code"
-                  name="postalCode"
                   value={postalCode}
+                  name="postalCode"
                   onChange={(ev) => setPostalCode(ev.target.value)}
                 />
               </CityHolder>
               <Input
                 type="text"
                 placeholder="Street Address"
-                name="streetAddress"
                 value={streetAddress}
+                name="streetAddress"
                 onChange={(ev) => setStreetAddress(ev.target.value)}
               />
               <Input
                 type="text"
                 placeholder="Country"
-                name="country"
                 value={country}
+                name="country"
                 onChange={(ev) => setCountry(ev.target.value)}
               />
-
-              <Button black={1} block={1} onClick={goToPayment}>
+              <Button black block onClick={goToPayment}>
                 Continue to payment
               </Button>
             </Box>
