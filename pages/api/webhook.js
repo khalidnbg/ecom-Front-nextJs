e200151d6c8af3de6f5fefc136aae30003011976
unknown <khalidnabgaoui4@gmail.com>
2024-08-13 12:16:35 +0100

    all products page

---
+++ pages/api/webhook.js
@@ -0,0 +1,50 @@
+import { mongooseConnect } from "@/lib/mongoose";
+const stripe = require("stripe")(process.env.STRIPE_SK);
+import { buffer } from "micro";
+import { Order } from "@/models/Order";
+
+const endpointSecret =
+  "whsec_3867868737ede2543fcbd21b1b773d0da8c749c7719e7c23ef889623f39d109a";
+
+export default async function handler(req, res) {
+  await mongooseConnect();
+  const sig = req.headers["stripe-signature"];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      await buffer(req),
+      sig,
+      endpointSecret
+    );
+  } catch (err) {
+    res.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case "checkout.session.completed":
+      const data = event.data.object;
+      const orderId = data.metadata.orderId;
+      const paid = data.payment_status === "paid";
+      if (orderId && paid) {
+        await Order.findByIdAndUpdate(orderId, {
+          paid: true,
+        });
+      }
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  res.status(200).send("ok");
+}
+
+export const config = {
+  api: { bodyParser: false },
+};
+
+// swift-prize-cute-valor
+// acct_1O5sM8AyihZG2601
